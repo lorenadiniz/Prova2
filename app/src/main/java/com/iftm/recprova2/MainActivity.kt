2e09b08cd349178843f386b3d5f20a596ef45564
Lorenadiniz <lorena.martins.1600@gmail.com>
2021-01-12 21:07:48 -0300

    commit

---
+++ app/src/main/java/com/iftm/recprova2/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.iftm.recprova2
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+       // var adiciona: Button? = null
+        //val diminui: Button? = null
+        //val zera: Button? = null
+        //val contador: TextView? = null
+       // val t = 0
+
+    }
+}
---
+++ app/src/main/java/com/iftm/recprova2/Soma.kt
@@ -0,0 +1,17 @@
+package com.iftm.recprova2
+
+import android.view.TextureView
+import android.view.View
+
+class Soma (itemView: View){
+    val adiciona: TextureView?=itemView.findViewById(R.id.bt_soma)
+    val diminui : TextureView?=itemView.findViewById(R.id.bt_sub)
+    val zerar : TextureView?=itemView.findViewById(R.id.bt_zera)
+    val contador : TextureView?=itemView.findViewById(R.id.cont)
+
+
+}
+
+fun clickNoBotao(view: View?) {
+
+}
